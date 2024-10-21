98247fbb79f1cdc48ae8e32ec3687cc3d314e82b
Iflaq khurshid <112414810+Iflaqbhat@users.noreply.github.com>
2024-10-21 16:50:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+	let fn=document.getElementById("fn").value;
+	let ln=document.getElementById("ln").value;
+	 let ans=fn+" "+ln;
+	alert(ans)
 }
